1baf44c1f3becf25ed7a55f1d6ffa05dc28a011a
deadrat <jay@covai.org>
2026-09-07 20:13:28 +0530

    fix: derive extension version dynamically from package.json in wxt.config

--- wxt.config.ts
+++ wxt.config.ts
@@ -20,7 +20,6 @@ export default defineConfig({
     default_locale: 'en',
     name: '__MSG_extName__',
     description: '__MSG_extDescription__',
-    version: '1.6.0',
     homepage_url: 'https://decant.covai.org/',
     permissions:
       browser === 'firefox'
